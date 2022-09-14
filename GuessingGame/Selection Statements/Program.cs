32b924e6f429f6ce143208c91af4cc4cdbf4e6d8
Ben Cofield <benrcofield@gmail.com>
2022-09-14 15:57:15 -0400

    First commit

---
+++ GuessingGame/Selection Statements/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace GuessingGame
+{
+    class Program
+    {
+        static void Main()
+        {
+            var randomNum = new Random();
+            var favoriteNum = randomNum.Next(1, 10);
+
+            while(true)
+            {
+                Console.Write("Guess a number between 1 and 10: ");
+                var guess = int.Parse(Console.ReadLine());
+            
+                if (guess > favoriteNum)
+                {
+                    Console.WriteLine("Too high");
+                }
+                else if (guess < favoriteNum)
+                {
+                    Console.WriteLine("Too low");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed correctly!");
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ SchoolSubjects/SchoolSubjects/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace SchoolSubjects
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("What is your favorite school subject?: ");
+            var userInput = Console.ReadLine();
+
+            switch(userInput.ToLower()) {
+                case "math":
+                    Console.WriteLine("You seem like a math wiz");
+                    break;
+                case "science":
+                    Console.WriteLine("Next nobel prize winner eh?");
+                    break;
+                case "history":
+                    Console.WriteLine("I like history too. Most of it is death and destruction");
+                    break;
+                case "english":
+                    Console.WriteLine("Oh you like English? Name every word");
+                    break;
+                case "music":
+                    Console.WriteLine("Can you play your scales for me?");
+                    break;
+                default:
+                    Console.WriteLine("Sorry I didn't get that");
+                    break;
+            }
+            
+        }
+    }
+}
